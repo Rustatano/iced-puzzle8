f989bad1443d5fb602d667aea770de90f64b89ba
Rustatano <jakub-svoboda111@seznam.cz>
2025-01-17 16:13:08 +0100

    learning the layout system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use number8::models::counter::Counter;
+use number8::models::grid::Grid;
 
 pub fn main() -> iced::Result {
-    iced::run("A cool counter", Counter::update, Counter::view)
+    iced::run("A cool counter", Grid::update, Grid::view)
 }

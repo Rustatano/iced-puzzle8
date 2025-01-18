0f6f9c2e5b8aaa226a0cc225cb7ec397ea46a70c
Rustatano <jakub-svoboda111@seznam.cz>
2025-01-18 14:07:37 +0100

    moved generate method

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use iced::Alignment::Center;
 use iced::Length::Fill;
-use iced::widget::{button, center, column, horizontal_space, row, scrollable, text};
+use iced::widget::{
+    button, center, column, container, horizontal_space, row, scrollable, slider, text,
+};
 use iced::{Element, Size, Task, Theme, widget};
 use rand::seq::SliceRandom;
 
@@ -17,14 +20,16 @@ pub fn main() -> iced::Result {
 pub enum Message {
     Generate,
     Solve,
+    ChangeGridSize(u8),
 }
 
 struct App {
-    grid_values: Vec<Vec<i32>>,
+    grid_values: Vec<Vec<u8>>,
     theme: Theme,
     button_heigth: f32,
     button_width: f32,
     grid_element_size: f32,
+    grid_size: u8,
 }
 
 impl App {
@@ -32,11 +37,12 @@ impl App {
         let theme = Theme::Moonfly;
         (
             Self {
-                grid_values: Self::generate(),
+                grid_values: generate(3),
                 theme,
                 button_heigth: 50.0,
                 button_width: 200.0,
                 grid_element_size: 80.0,
+                grid_size: 3,
             },
             widget::focus_next(),
         )
@@ -45,15 +51,23 @@ impl App {
     fn update(&mut self, message: Message) {
         match message {
             Message::Generate => {
-                self.grid_values = Self::generate();
+                self.grid_values = generate(self.grid_size);
             }
 
             Message::Solve => self.solve(),
+            Message::ChangeGridSize(size) => {
+                self.grid_size = size;
+            }
         }
     }
 
     fn view(&self) -> Element<Message> {
         let control_panel = column![
+            slider(2..=4, self.grid_size, Message::ChangeGridSize)
+                .step(1)
+                .default(3)
+                .width(self.button_width),
+            text("grid size: ".to_owned() + &self.grid_size.to_string()).size(20),
             button(text("Generate").size(30))
                 .on_press(Message::Generate)
                 .width(self.button_width)
@@ -73,41 +87,63 @@ impl App {
         .spacing(30)
         .padding(20);
 
-        let row1 = row![
-            text(self.grid_values[0][0]).size(self.grid_element_size),
-            text(self.grid_values[0][1]).size(self.grid_element_size),
-            text(self.grid_values[0][2]).size(self.grid_element_size),
-        ];
-
-        let row2 = row![
-            text(self.grid_values[1][0]).size(self.grid_element_size),
-            text(self.grid_values[1][1]).size(self.grid_element_size),
-            text(self.grid_values[1][2]).size(self.grid_element_size),
-        ];
-
-        let row3 = row![
-            text(self.grid_values[2][0]).size(self.grid_element_size),
-            text(self.grid_values[2][1]).size(self.grid_element_size),
-            text(self.grid_values[2][2]).size(self.grid_element_size),
-        ];
-
-        let grid = column![row1, row2, row3].padding(20);
+        let grid = container(column![
+            row![
+                text(self.grid_values[0][0])
+                    .size(self.grid_element_size)
+                    .width(self.grid_element_size)
+                    .align_x(Center)
+                    .align_y(Center),
+                text(self.grid_values[0][1])
+                    .size(self.grid_element_size)
+                    .width(self.grid_element_size)
+                    .align_x(Center)
+                    .align_y(Center),
+                text(self.grid_values[0][2])
+                    .size(self.grid_element_size)
+                    .width(self.grid_element_size)
+                    .align_x(Center)
+                    .align_y(Center),
+            ],
+            row![
+                text(self.grid_values[1][0])
+                    .size(self.grid_element_size)
+                    .width(self.grid_element_size)
+                    .align_x(Center)
+                    .align_y(Center),
+                text(self.grid_values[1][1])
+                    .size(self.grid_element_size)
+                    .width(self.grid_element_size)
+                    .align_x(Center)
+                    .align_y(Center),
+                text(self.grid_values[1][2])
+                    .size(self.grid_element_size)
+                    .width(self.grid_element_size)
+                    .align_x(Center)
+                    .align_y(Center),
+            ],
+            row![
+                text(self.grid_values[2][0])
+                    .size(self.grid_element_size)
+                    .width(self.grid_element_size)
+                    .align_x(Center)
+                    .align_y(Center),
+                text(self.grid_values[2][1])
+                    .size(self.grid_element_size)
+                    .width(self.grid_element_size)
+                    .align_x(Center)
+                    .align_y(Center),
+                text(self.grid_values[2][2])
+                    .size(self.grid_element_size)
+                    .width(self.grid_element_size)
+                    .align_x(Center)
+                    .align_y(Center),
+            ]
+        ])
+        .padding(20)
+        .style(container::dark);
 
         center(row![control_panel, horizontal_space().width(Fill), grid,].spacing(20)).into()
-    }
-
-    fn generate() -> Vec<Vec<i32>> {
-        let mut rng = rand::thread_rng();
-        let mut arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-        let mut out: Vec<Vec<i32>> = vec![vec![0, 0, 0,], vec![0, 0, 0,], vec![0, 0, 0,],];
-        arr.shuffle(&mut rng);
-
-        for i in 0..3 {
-            for j in 0..3 {
-                out[i][j] = arr[i * 3 + j];
-            }
-        }
-        out
     }
 
     fn solve(&mut self) {
@@ -117,4 +153,24 @@ impl App {
     fn theme(&self) -> Theme {
         self.theme.clone()
     }
+}
+
+fn generate(grid_size: u8) -> Vec<Vec<u8>> {
+    let mut rng = rand::thread_rng();
+    let mut arr = vec![];
+    for i in 0..grid_size * grid_size {
+        arr.push(i);
+    }
+    let mut out = vec![];
+    arr.shuffle(&mut rng);
+
+    for i in 0..grid_size {
+        out.push(vec![]);
+        for j in 0..grid_size {
+            out[i as usize].push(0);
+            out[i as usize][j as usize] = arr[(i * grid_size + j) as usize];
+        }
+    }
+
+    out
 }
